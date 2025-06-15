3b32159fa147a5310bde7b19d5d5f2ffc97fe2cd
sobaina <27100398@lums.edu.pk>
2025-06-15 23:17:46 +0500

    Add files via upload
    Portfolio.html contains the website code while input.css has the stylings

---
+++ profile.js
@@ -0,0 +1,7 @@
+const input_value = document.getElementById('input');
+input_value.value = 'entered';
+
+const button = document.getElementById("mybutton")
+button.addEventListener("add here", function () {
+    alert("addeed");
+});
