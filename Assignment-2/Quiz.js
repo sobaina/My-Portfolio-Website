76b20af30e1414cadb17abff0ec85606518f5f98
sobaina <27100398@lums.edu.pk>
2025-06-26 00:08:29 +0500

    Add Assignment 2 to portfolio repo

---
+++ Assignment-2/Quiz.js
@@ -0,0 +1,43 @@
+const button = document.querySelector("#my-button")
+
+button.addEventListener("click", () => {
+    const input = document.getElementById("text-input") //input feild created 
+    //   the question statement that user added 
+    const new_task = input.value
+    //the question
+    const new_element = document.createElement("p") //create eleemnt by giving tag name
+    new_element.innerText = new_task
+    //the 4 options appear 
+    const new_element1 = document.createElement("input")
+    new_element1.placeholder = "Add Option 1"
+    new_element1.createElement("button")
+    const new_element2 = document.createElement("input") //create eleemnt by giving tag name
+    new_element2.placeholder = "Add Option 2"
+    const new_element3 = document.createElement("input") //create eleemnt by giving tag name
+    new_element3.placeholder = "Add Option 3"
+    const new_element4 = document.createElement("input") //create eleemnt by giving tag name
+    new_element4.placeholder = "Add Option 4"
+    //now add this  question statement to make a box
+
+    //select which element should the children list be added to 
+    new_element.classList.add("task-item")
+    //options
+    new_element1.classList.add("task-item")
+    new_element2.classList.add("task-item")
+    new_element3.classList.add("task-item")
+    new_element4.classList.add("task-item")
+    //for delete button
+
+    const lists = document.querySelector("#listofoptions")
+    lists.appendChild(new_element)
+    lists.appendChild(new_element1)
+    lists.appendChild(new_element2)
+    lists.appendChild(new_element3)
+    lists.appendChild(new_element4)
+
+
+
+
+    input.value = ""
+
+})
