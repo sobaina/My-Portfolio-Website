a7bc0fce824f5ae0c8331bce524ecd7622a659ab
sobaina <27100398@lums.edu.pk>
2025-07-02 21:54:49 +0500

    made seperate folders

--- profile.js
+++
@@ -1,7 +0,0 @@
-const input_value = document.getElementById('input');
-input_value.value = 'entered';
-
-const button = document.getElementById("mybutton")
-button.addEventListener("add here", function () {
-    alert("addeed");
-});
